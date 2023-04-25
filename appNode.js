81d1d36f21d5c64c5b2a67220097092a93845a5b
CaioHenriqueCastro <caiohenriqueestevesdecastro@gmail.com>
2023-04-24 21:33:24 -0300

    huhuh

---
+++ appNode.js
@@ -0,0 +1,16 @@
+// protocolos da internet 
+const http = require('http');
+
+//ip e porta do servidor nodejs
+const hostname = '127.0.0.1'; // localhost
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Bienvenido hijo de puta');
+});
+// servidor escutando na porta...
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
